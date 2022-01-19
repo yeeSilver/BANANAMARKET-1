cbcdffe79f869b9ac1ca724766c3adedfb4a3a1a
SongyeonHan <93498183+SongyeonHan@users.noreply.github.com>
2022-01-19 11:17:02 +0900

    개인프로필 불러오기

--- js/userpage.js
+++ js/userpage.js
@@ -1,3 +1,66 @@
+// 로그인한 프로필 가져오기
+async function getProfile() {
+  const accountname = localStorage.getItem("accountname")
+
+  const url = `http://146.56.183.55:5050/profile/${accountname}`
+  const token = localStorage.getItem("Token")
+  const res = await fetch(url,{
+      method:"GET",
+      headers:{
+          "Authorization" : `Bearer ${token}`,
+          "Content-type" : "application/json"
+      }
+  })
+  console.log(res);
+  const json = await res.json()
+  console.log(json)
+  console.log("=-=-=-=-=-=-=-이 위는 개인프로필 정보입니다.=-=-=-=-=-=-=-=-=-=-=-=")
+  const 이미지 = json.profile.image
+  const 이름 = json.profile.username
+  const 계정 = json.profile.accountname
+  const 소개 = json.profile.intro
+  const 팔로워수 = json.profile.followerCount
+  const 팔로잉수 = json.profile.followingCount
+
+  console.log(계정)
+  
+  document.querySelector(".profile").innerHTML+=`
+  <a href="" class="display-inline basic-profile"><img src="${이미지}" alt=""></a>
+
+  <div class="profile-desc">
+  <h2>${이름}</h2>
+  <small>@ ${계정}</small>
+  <p>${소개}</p>
+  </div>
+
+  <div class="btn-set">
+    <button type="submit" class="display-inline unfollow-btn">프로필 수정</button>
+    <button type="submit" class="display-inline unfollow-btn">상품 등록</button>
+  </div>
+    `
+
+  document.querySelector(".followers-num").innerHTML+=`
+    <p class="followBtn">${팔로워수}</p>
+    <small>followers</small>
+  `
+
+  document.querySelector(".followings-num").innerHTML+=`
+  <p class="followingBtn">${팔로잉수}</p>
+  <small>followers</small>
+`
+}
+
+getProfile()
+
+const 팔로워 = document.querySelector('.followBtn')
+const 팔로잉 = document.querySelector('.followingBtn')
+
+팔로워.addEventListener('click', function() {
+location.href = "./followerlist.html"
+})
+팔로잉.addEventListener('click', function() {
+location.href = "./followerlist.html"
+})
 
 const sellDiv = document.querySelector('.sell-items');
 
@@ -37,3 +100,4 @@ async function GetSaleInfo(){
     sellDiv.appendChild(sellArt);
   })
 }
+
