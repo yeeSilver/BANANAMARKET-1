afcc1c20b5b1af227f0f2b974c47684ab75c0245
yeeun_An <an446@naver.com>
2022-01-20 14:32:35 +0900

    :art: userpage : list형식으로 피드 출력 구현

--- js/userpage.js
+++ js/userpage.js
@@ -106,6 +106,10 @@ const listBtn = document.querySelector(".show-list img");
 const albumSec = document.querySelector(".album");
 const listSec = document.querySelector(".home-feed");
 
+window.onload = function () {
+  albumSec.classList.add("hide");
+  GetList();
+};
 albumBtn.addEventListener("click", () => {
   albumBtn.src = "./img/icon-post-album-on.png";
   listBtn.src = "./img/icon-post-list-off.png";
@@ -126,8 +130,7 @@ listBtn.addEventListener("click", () => {
 async function GetList() {
   const token = localStorage.getItem("Token");
   const accountname = localStorage.getItem("accountname");
-  const albumPhotoDiv = document.querySelector(".album-photos");
-  const albumimgdata = await fetch(
+  const feedimgdata = await fetch(
     `http://146.56.183.55:5050/post/${accountname}/userpost`,
     {
       method: "GET",
@@ -137,22 +140,78 @@ async function GetList() {
       },
     }
   );
-  const album = await albumimgdata.json();
-  const albumPost = album.post;
-  albumPost.forEach((el) => {
-    const imgsrc = el.image;
-    console.log(imgsrc);
-    let albumDiv = document.createElement("div");
-    albumDiv.innerHTML = `
-    <div>
-      <a href="">
-      <img src="${imgsrc}" alt="">
-      </a>
+  const list = await feedimgdata.json();
+  const listPost = list.post;
+  listPost.forEach((el) => {
+    const username = el.author.username;
+    const userImg = el.author.image;
+    const accountname = el.author.accountname;
+    const content = el.content;
+    const feedImg = el.image;
+    let updateAt = el.updatedAt;
+    updateAt = updateAt.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/g);
+    console.log(typeof updateAt);
+    console.log(updateAt.join());
+    // let updateAts = updateAt.join();
+    // updateAts = updateAt.split("-");
+    let heartCount = 0;
+    let commentCount = 0;
+    if (el.hearted && el.comments) {
+      heartCount = el.heartCount;
+      commentCount = el.comments.length;
+    }
+    console.log(el);
+
+    let feedArt = document.createElement("article");
+    feedArt.classList.add("art-post");
+
+    feedArt.innerHTML = `
+      <div class="post-user">
+      <img
+        src="${userImg}"
+        alt=""
+        class="img-mini-profile"
+      />
+      <div>
+        <h2 class="post-title">${username}</h2>
+        <p class="post-user-id">@${accountname}</p>
+      </div>
+      <button>
+        <img id="more" src="./img/more-vertical.png" alt="" />
+      </button>
     </div>
-    `;
-    albumPhotoDiv.appendChild(albumDiv);
+    <!-- 포스트 메인-->
+    <div class="post-main">
+      <!-- 글 -->
+      <div class="con-post-main">
+        <p>
+          ${content}
+        </p>
+        <img src="${feedImg}" alt="" />
+      </div>
+      <!-- likes -->
+      <div class="con-reaction">
+        <!-- likes & comments -->
+        <ul class="list-reaction">
+          <li class="likes">
+            <img src="./img/2/heart.svg" alt="" />
+            <span class="number">${heartCount}</span>
+          </li>
+          <li class="comments">
+            <img src="./img/2/footer-icon/chat.svg" alt="" />
+            <span class="number">${commentCount}</span>
+          </li>
+        </ul>
+        <!-- 업로드 날짜 -->
+        <p class="post-date">${updateAt} </p>
+        </div>
+        </div>
+        `;
+    listSec.appendChild(feedArt);
   });
 }
+// <p class="post-date">${updateAts[0]}년 ${updateAts[1]}월 ${updateAts[2]}일 </p>
+// <p class="post-date">${updateAts[0]}년 ${updateAts[1]}월 ${updateAts[2]}일 </p>
 
 // 피드 가져오기 앨범형식
 async function GetAlbum() {
