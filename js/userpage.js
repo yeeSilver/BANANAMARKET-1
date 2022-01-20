7aa373f619432dd66d88a2db304a8ec66d22d575
yeeun_An <an446@naver.com>
2022-01-20 09:37:51 +0900

    :bug: userpage.js 변수 지정문

--- js/userpage.js
+++ js/userpage.js
@@ -1,103 +1,103 @@
 // 로그인한 프로필 가져오기
 async function getProfile() {
-  const accountname = localStorage.getItem("accountname")
-
-  const url = `http://146.56.183.55:5050/profile/${accountname}`
-  const token = localStorage.getItem("Token")
-  const res = await fetch(url,{
-      method:"GET",
-      headers:{
-          "Authorization" : `Bearer ${token}`,
-          "Content-type" : "application/json"
-      }
-  })
+  const accountname = localStorage.getItem("accountname");
+  const url = `http://146.56.183.55:5050/profile/${accountname}`;
+  const token = localStorage.getItem("Token");
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+  });
   console.log(res);
-  const json = await res.json()
-  console.log(json)
-  console.log("=-=-=-=-=-=-=-이 위는 개인프로필 정보입니다.=-=-=-=-=-=-=-=-=-=-=-=")
-  const 이미지 = json.profile.image
-  const 이름 = json.profile.username
-  const 계정 = json.profile.accountname
-  const 소개 = json.profile.intro
-  const 팔로워수 = json.profile.followerCount
-  const 팔로잉수 = json.profile.followingCount
+  const json = await res.json();
+  console.log(json);
+  console.log(
+    "=-=-=-=-=-=-=-이 위는 개인프로필 정보입니다.=-=-=-=-=-=-=-=-=-=-=-="
+  );
+  const 이미지 = json.profile.image;
+  const 이름 = json.profile.username;
+  const 계정 = json.profile.accountname;
+  const 소개 = json.profile.intro;
+  const 팔로워수 = json.profile.followerCount;
+  const 팔로잉수 = json.profile.followingCount;
 
-  console.log(계정)
-  
-  document.querySelector(".profile").innerHTML+=`
+  console.log(계정);
+
+  document.querySelector(".profile").innerHTML += `
   <a href="" class="display-inline basic-profile"><img src="${이미지}" alt=""></a>
-
   <div class="profile-desc">
   <h2>${이름}</h2>
   <small>@ ${계정}</small>
   <p>${소개}</p>
   </div>
-
   <div class="btn-set">
     <button type="submit" class="display-inline unfollow-btn">프로필 수정</button>
     <button type="submit" class="display-inline unfollow-btn">상품 등록</button>
   </div>
-    `
+    `;
 
-  document.querySelector(".followers-num").innerHTML+=`
+  document.querySelector(".followers-num").innerHTML += `
     <p class="followBtn">${팔로워수}</p>
     <small>followers</small>
-  `
+  `;
 
-  document.querySelector(".followings-num").innerHTML+=`
+  document.querySelector(".followings-num").innerHTML += `
   <p class="followingBtn">${팔로잉수}</p>
   <small>followers</small>
-`
+`;
+  const 팔로워 = document.querySelector(".followBtn");
+  const 팔로잉 = document.querySelector(".followingBtn");
+  console.log(팔로워);
+  팔로워.addEventListener("click", function () {
+    location.href = "./followerlist.html";
+  });
+  팔로잉.addEventListener("click", function () {
+    location.href = "./followerlist.html";
+  });
 }
 
-getProfile()
+getProfile();
 
-const 팔로워 = document.querySelector('.followBtn')
-const 팔로잉 = document.querySelector('.followingBtn')
+// 판매 게시글 가져오는 파트
+const sellDiv = document.querySelector(".sell-items");
 
-팔로워.addEventListener('click', function() {
-location.href = "./followerlist.html"
-})
-팔로잉.addEventListener('click', function() {
-location.href = "./followerlist.html"
-})
-
-const sellDiv = document.querySelector('.sell-items');
-
-window.onload = function(){
+window.onload = function () {
   GetSaleInfo();
-}
+};
 // 판매 게시글 가져오기
-async function GetSaleInfo(){
-  const token = localStorage.getItem("Token")
-  const accountname = localStorage.getItem("accountname")
-  const saleimgdata = await fetch(`http://146.56.183.55:5050/product/${accountname}`, {
-    method: "GET",
-    headers:{
-      "Authorization" : `Bearer ${token}`,
-      "Content-type" : "application/json"
+async function GetSaleInfo() {
+  const token = localStorage.getItem("Token");
+  const accountname = localStorage.getItem("accountname");
+  const saleimgdata = await fetch(
+    `http://146.56.183.55:5050/product/${accountname}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
     }
-  })
+  );
   const salejson = await saleimgdata.json();
   const sale_pro = salejson.product;
   console.log(salejson);
-  sale_pro.forEach((el)=>{
+  sale_pro.forEach((el) => {
     const itemName = el.itemName;
     const itemImg = el.itemImage;
     const itemLink = el.link;
     const itemPrice = el.price;
     console.log(itemImg);
-    let sellArt = document.createElement('article');
-    sellArt.classList.add('display-sell');
-    sellArt.innerHTML = 
-    `
+    let sellArt = document.createElement("article");
+    sellArt.classList.add("display-sell");
+    sellArt.innerHTML = `
       <a href="${itemLink}">
         <img src="${itemImg}" alt="">
         <p>${itemName}</p>
         <small>${itemPrice}원</small>
       </a>
-    `
+    `;
     sellDiv.appendChild(sellArt);
-  })
+  });
 }
-
